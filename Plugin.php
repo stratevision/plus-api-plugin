aa36aaccee17a728c5005c68d770904eb8c78bd3
jacksun101 <sunhsilu@gmail.com>
2026-03-10 15:43:28 +0800

    cors 在新的 laravel 已經是內建

--- Plugin.php
+++ Plugin.php
@@ -8,11 +8,11 @@ class Plugin extends PluginBase
     public function boot()
     {
         // Register Cors
-        App::register('\Fruitcake\Cors\CorsServiceProvider');
+        // App::register('\Fruitcake\Cors\CorsServiceProvider');
 
         // Add cors middleware
-        $this->app['Illuminate\Contracts\Http\Kernel']
-            ->prependMiddleware(\Fruitcake\Cors\HandleCors::class);
+        // $this->app['Illuminate\Contracts\Http\Kernel']
+        //     ->prependMiddleware(\Fruitcake\Cors\HandleCors::class);
 
     }
 
